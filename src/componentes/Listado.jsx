452cd6aad33e1ae5508c1735e53a604e71bd47eb
Hector <mariaalejandrafigueroasantosz@gmail.com>
2022-08-12 10:40:04 +0100

    Añadir rafce a el componente Listado.jsx x2

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Listado = () => {
   return (
-	<div><p class='añadir cambios a git'>Listado</p></div>
+	<div><p class='añadir cambios a git'><p class="Segundo cambio porque no me di cuenta de que el ejercicio 7 solo pedía modificar uno">Listado</p></p></div>
 
   )
 }
